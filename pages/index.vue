e6315e903f56f090b9158457fbc1825700a202e2
Gustavo Moreno <gustavo@fosterapps.com>
2020-05-01 20:16:45 -0500

    add file dokku 14

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,10 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">template prodution</h1>
+      <h1 class="title">Komercia</h1>
       <h2 class="subtitle">My legendary Nuxt.js project</h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </div>
