e3fad99c998fba5edc4266f523b2acb673405d62
miguel3d <45762328+Miguel3D@users.noreply.github.com>
2020-06-25 17:49:34 -0500

    server

--- nuxt.config.js
+++ nuxt.config.js
@@ -100,13 +100,8 @@ export default {
   build: {
     transpile: ['vee-validate/dist/rules'],
     extend(config, ctx) {
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/,
-        })
+      if (ctx.isDev) {
+        config.devtool = ctx.isClient ? 'source-map' : 'inline-source-map'
       }
     },
   },
