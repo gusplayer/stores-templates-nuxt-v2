11c697b6cc0768e1472a35b06a01290c87874486
Gustavo Moreno <gustavo@fosterapps.com>
2020-06-25 17:25:45 -0500

    put https

--- store/index.js
+++ store/index.js
@@ -424,22 +424,18 @@ export const actions = {
     let full = req.headers.host
     let parts = full.split('.')
     let subdomain = parts[0]
-    // console.log(parts[1])
 
-    let id = 0
+    let id = 582
 
-    if (parts[1] == 'komercia' || parts[1] == 'localhost:3000') {
-      id = await axios.post(`https://api2.komercia.co/api/tienda/info/by/url`, {
-        name: `${subdomain}.komercia.co/`,
-      })
-    } else {
-      // id = await axios.post(`https://api2.komercia.co/api/tienda/info/by/url`, {
-      //   name: `${subdomain}.komercia.co/`,
-      // })
-      id = await axios.post(`https://api2.komercia.co/api/tienda/info/by/url`, {
-        name: `https://${full}`,
-      })
-    }
+    // if (parts[1] == 'komercia' || parts[1] == 'localhost:3000') {
+    //   id = await axios.post(`https://api2.komercia.co/api/tienda/info/by/url`, {
+    //     name: `${subdomain}.komercia.co/`,
+    //   })
+    // } else {
+    //   id = await axios.post(`https://api2.komercia.co/api/tienda/info/by/url`, {
+    //     name: `https://${full}`,
+    //   })
+    // }
 
     // console.log(id)
 
