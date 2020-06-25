c1b6121e8c45fc6f683da41ca8c349c21464d4d0
miguel3d <45762328+Miguel3D@users.noreply.github.com>
2020-06-25 15:42:58 -0500

    server

--- nuxt.config.js
+++ nuxt.config.js
@@ -94,11 +94,37 @@ export default {
   buildDir: '_nuxt',
   build: {
     publicPath: '/_nuxt/dist/',
-    transpile: ['vee-validate/dist/rules'],
+    filenames: {
+      app: ({ isDev }) => (isDev ? '[name].js' : '[chunkhash].js'),
+      chunk: ({ isDev }) => (isDev ? '[name].js' : '[chunkhash].js'),
+      css: ({ isDev }) => (isDev ? '[name].css' : '[contenthash].css'),
+      img: ({ isDev }) => (isDev ? '[path][name].[ext]' : 'img/[hash:7].[ext]'),
+      font: ({ isDev }) =>
+        isDev ? '[path][name].[ext]' : 'fonts/[hash:7].[ext]',
+      video: ({ isDev }) =>
+        isDev ? '[path][name].[ext]' : 'videos/[hash:7].[ext]',
+    },
     extend(config, ctx) {
-      if (ctx.isDev) {
-        config.devtool = ctx.isClient ? 'source-map' : 'inline-source-map'
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+          options: {
+            fix: true,
+          },
+        })
       }
     },
   },
+  // build: {
+  //   publicPath: '/_nuxt/dist/',
+  //   transpile: ['vee-validate/dist/rules'],
+  //   extend(config, ctx) {
+  //     if (ctx.isDev) {
+  //       config.devtool = ctx.isClient ? 'source-map' : 'inline-source-map'
+  //     }
+  //   },
+  // },
 }
