0914127700568e73181104611664c6ce3e95edde
Gustavo Moreno <gustavo@fosterapps.com>
2020-07-08 19:46:54 -0500

    refactor

--- plugins/amplify.js
+++
@@ -1,6 +0,0 @@
-//plugins/amplify.js
-import Vue from 'vue'
-import Amplify, * as AmplifyModules from 'aws-amplify'
-import { AmplifyPlugin } from 'aws-amplify-vue'
-
-Vue.use(AmplifyPlugin, AmplifyModules)
