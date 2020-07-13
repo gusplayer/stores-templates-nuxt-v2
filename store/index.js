1514ba70019b895a5cd687c317dd6735dc0ac39d
Gustavo Moreno <gustavo@fosterapps.com>
2020-07-13 11:48:18 -0500

    debug router patch

--- store/index.js
+++ store/index.js
@@ -443,7 +443,7 @@ export const actions = {
     }
     //await dispatch('GET_DATA_TIENDA_BY_ID', id.data.data.id)
     await dispatch('GET_DATA_TIENDA_BY_ID', '582')
-    dispatch('GET_SERVER_PATH', 'melissa2')
+    dispatch('GET_SERVER_PATH', full)
 
     const idSlug = route.path.split('-')
     const producto = await axios.get(
