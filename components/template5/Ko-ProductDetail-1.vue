34ddd2e03f5c0401347746d7044f9610b14ccd29
miguel3d <45762328+Miguel3D@users.noreply.github.com>
2020-11-06 19:10:50 -0500

    Test Microdata Tags Facebook

--- components/template5/Ko-ProductDetail-1.vue
+++ components/template5/Ko-ProductDetail-1.vue
@@ -306,6 +306,16 @@
         </div>
       </div>
     </div>
+    <div itemtype="http://schema.org/Product">
+      <meta itemprop="productID" :content="`${data.detalle.id}`" />
+      <meta itemprop="name" :content="`${data.detalle.nombre}`" />
+      <meta itemprop="description" content="Producto" />
+      <meta itemprop="image" :content="`${data.detalle.foto_cloudinary}`" />
+      <meta itemprop="price" :content="`${data.detalle.precio}`" />
+      <meta itemprop="priceCurrency" :content="`${data.detalle.nombre}`" />
+      <meta itemprop="availability" :content="`${data.detalle.stock}`" />
+      <meta itemprop="itemCondition" content="New" />
+    </div>
   </div>
 </template>
 
