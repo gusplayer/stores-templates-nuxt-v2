28efe84f916917fdabb09fea43b9c3f103621438
miguel3d <45762328+Miguel3D@users.noreply.github.com>
2021-02-15 10:48:51 -0500

    Google tagmanager

--- plugins/ga.js
+++ plugins/ga.js
@@ -1,53 +1,56 @@
+import Vue from 'vue'
+import VueGtag from 'vue-gtag'
+
 export default ({ app, store }) => {
   let analytics =
     store.state.analytics_tagmanager &&
     store.state.analytics_tagmanager.analytics
       ? store.state.analytics_tagmanager.analytics
       : ''
-
-  if (process.env.NODE_ENV !== 'production')
-    return /*
-     ** Include Google Analytics Script
-     */
-  ;(function (i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r
-    ;(i[r] =
-      i[r] ||
-      function () {
-        ;(i[r].q = i[r].q || []).push(arguments)
-      }),
-      (i[r].l = 1 * new Date())
-    ;(a = s.createElement(o)), (m = s.getElementsByTagName(o)[0])
-    a.async = 1
-    a.src = g
-    m.parentNode.insertBefore(a, m)
-  })(
-    window,
-    document,
-    'script',
-    'https://www.google-analytics.com/analytics.js',
-    'ga'
-  )
-  /*
-   ** Set the current page
-   */
-  ga('create', analytics, 'auto')
-  ga('create', 'UA-92934137-1', 'auto', 'clientTracker')
-  /*
-   ** Every time the route changes (fired on initialization too)
-   */
-  app.router.afterEach((to, from) => {
-    /*
-     ** We tell Google Analytics to add a `pageview`
-     */
-    ga('set', 'page', to.fullPath)
-    ga('send', 'pageview')
-    // ga('clientTracker.send', 'pageview')
-    // ga('send', 'pageview', {
-    //   page: location.host + location.pathname,
-    // })
-    ga('clientTracker.send', 'pageview', {
-      page: location.host + location.pathname,
+  let result = analytics.split('-')
+  // if (process.env.NODE_ENV !== 'production') return
+  if (result[0] == 'UA') {
+    ;(function (i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r
+      ;(i[r] =
+        i[r] ||
+        function () {
+          ;(i[r].q = i[r].q || []).push(arguments)
+        }),
+        (i[r].l = 1 * new Date())
+      ;(a = s.createElement(o)), (m = s.getElementsByTagName(o)[0])
+      a.async = 1
+      a.src = g
+      m.parentNode.insertBefore(a, m)
+    })(
+      window,
+      document,
+      'script',
+      'https://www.google-analytics.com/analytics.js',
+      'ga'
+    )
+    ga('create', analytics, 'auto')
+    ga('create', 'UA-92934137-1', 'auto', 'clientTracker')
+    app.router.afterEach((to, from) => {
+      ga('set', 'page', to.fullPath)
+      ga('send', 'pageview')
+      ga('clientTracker.send', 'pageview', {
+        page: location.host + location.pathname,
+      })
     })
-  })
+  } else {
+    if (result[0] == 'G') {
+      Vue.use(VueGtag, {
+        config: { id: 'UA-92934137-1' },
+        includes: [
+          {
+            id: analytics,
+            params: {
+              send_page_view: false,
+            },
+          },
+        ],
+      })
+    }
+  }
 }
