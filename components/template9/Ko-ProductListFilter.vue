5457998ab24c113f0951067837dc9b8469d487cd
miguel3d <miguel@komercia.co>
2021-05-05 16:18:53 -0500

    git push origin master

--- components/template9/Ko-ProductListFilter.vue
+++ components/template9/Ko-ProductListFilter.vue
@@ -289,7 +289,6 @@ export default {
     }
     var acc = document.getElementsByClassName('accordion')
     var i
-
     for (i = 0; i < acc.length; i++) {
       acc[i].addEventListener('click', function () {
         this.classList.toggle('active')
--- mixins/idCloudinary.js
+++ mixins/idCloudinary.js
@@ -3,7 +3,7 @@ export default {
     idCloudinary(url, width, height) {
       let fitImage = url.split('/upload/')
       let fitImage2 = url.split('/')
-      return `https://res.cloudinary.com/${fitImage2[3]}/image/upload/c_crop,g_custom/h_${height},w_${width},b_auto,c_pad,dpr_auto,q_100,f_auto/${fitImage[1]}`
+      return `https://res.cloudinary.com/${fitImage2[3]}/image/upload/c_crop,g_custom/h_${height},w_${width},b_auto,c_pad,dpr_auto,q_auto,f_auto/${fitImage[1]}`
     },
     idCloudinaryDetalle(url, width, height) {
       let fitImage = url.split('/upload/')
@@ -18,7 +18,7 @@ export default {
     idCloudinaryBanner(url) {
       let fitImage = url.split('/upload/')
       let fitImage2 = url.split('/')
-      return `https://res.cloudinary.com/${fitImage2[3]}/image/upload/c_lpad,dpr_auto,q_100,f_auto,c_crop,g_custom/${fitImage[1]}`
+      return `https://res.cloudinary.com/${fitImage2[3]}/image/upload/c_lpad,dpr_auto,q_auto,f_auto,c_crop,g_custom/${fitImage[1]}`
     },
     idCloudinaryBannerResponsive(url, width) {
       let fitImage = url.split('/upload/')
