46646b4aad4e853f5d7d80c56a0846b4eff0297b
manAbl <manuel.22.lamas@gmail.com>
2021-06-01 19:37:38 -0500

    basic setup for the template12 page

---
+++ pages/template12/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="settingByTemplate12">
+    whatever components here
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'default',
+  components: {},
+  mounted() {
+    window.addEventListener('message', this.addEventListenertemplate)
+  },
+  computed: {
+    dataStore() {
+      return this.$store.state.dataStore
+    },
+    fullProducts() {
+      return this.$store.getters['products/filterProducts']
+    },
+    listArticulos() {
+      return this.$store.state.listArticulos
+    },
+    settingByTemplate12() {
+      return this.$store.state.settingByTemplate12
+    },
+  },
+  methods: {
+    addEventListenertemplate() {},
+  }
+}
+</script>
+
+<style>
+
+</style>
