b3340fdf80f9f5b81d46dca70286feb518b199f4
miguel3d <miguel@komercia.co>
2021-10-05 14:46:59 -0500

    improve contact template 11

--- components/template11/ko-Contact.vue
+++ components/template11/ko-Contact.vue
@@ -32,7 +32,10 @@
             <div class="content-info">
               <p class="txt-info">Información</p>
             </div>
-            <div class="content-locatioin">
+            <div
+              class="content-locatioin"
+              v-if="this.dataStore.geolocalizacion.length"
+            >
               <svg
                 class="icon-left"
                 xmlns="http://www.w3.org/2000/svg"
@@ -45,10 +48,7 @@
                   d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"
                 />
               </svg>
-              <p
-                class="txt-left"
-                v-if="this.dataStore.geolocalizacion[0].direccion"
-              >
+              <p class="txt-left">
                 {{ this.dataStore.geolocalizacion[0].direccion }}
               </p>
             </div>
