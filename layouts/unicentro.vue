fcbe173549051ddf86d2f4857bd13ed66ebbd2ae
Miguel <miguel.morenori@hotmail.com>
2022-06-02 23:13:11 -0500

    update template

--- layouts/unicentro.vue
+++ layouts/unicentro.vue
@@ -71,11 +71,11 @@ export default {
     KoFooter8: () => import('../components/footers/footer8/Ko-Footer-8'),
     KoFooterCountry: () =>
       import('../components/footers/footer1/Ko-Footer-Country'),
-    koWhatsApp: () => import('../components/whatsApp/whatsApp'),
+    koWhatsApp: () => import('../components/whatsapp/whatsApp.vue'),
     koTiendaCerrada: () => import('../assets/img/tiendaCerrada'),
     // Ko6Header1: () => import('../components/headers/header1/Ko6-Header-1'),
     // Ko6Footer1: () => import('../components/footers/footer1/Ko6-Footer-1'),
-    koTiendaError: () => import('../components/Ko-errorStore'),
+    koTiendaError: () => import('../components/Ko-errorStore.vue'),
     koModalSecurity: () => import('../components/modal/Ko-modal-security.vue'),
   },
   mounted() {
