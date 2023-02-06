c08528317fc0c80336a0bcf939ab5f1234e9bae5
Miguel Moreno <miguelito2094@hotmail.com>
2023-02-06 16:30:22 -0500

    update productCart wapi

--- components/whatsapp/template3/productCard/ko-productCard.vue
+++ components/whatsapp/template3/productCard/ko-productCard.vue
@@ -10,13 +10,13 @@
       >
         <img
           v-if="!soldOut"
-          v-lazy="idCloudinary(this.product.foto_cloudinary, 400, 400)"
+          v-lazy="idCloudinary(this.product.foto_cloudinary, 850, 850)"
           class="product-image"
           alt="Product-Img-soldOut"
         />
         <img
           v-if="soldOut"
-          v-lazy="idCloudinary(this.product.foto_cloudinary, 400, 400)"
+          v-lazy="idCloudinary(this.product.foto_cloudinary, 850, 850)"
           class="product-image product-image-soldOut"
           alt="Product-Img"
         />
