273e05aeb72f1473eded5ce324abd9d25b390692
Miguel3D <miguel.amorenori@gmail.com>
2023-12-26 12:34:39 -0500

    template 7 design update

--- components/template7/_carouselBanner/K07-BannerCarousel.vue
+++ components/template7/_carouselBanner/K07-BannerCarousel.vue
@@ -142,10 +142,10 @@ export default {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
         },
-        autoplay: {
-          delay: 6000,
-          disableOnInteraction: false,
-        },
+        // autoplay: {
+        //   delay: 6000,
+        //   disableOnInteraction: false,
+        // },
       },
     }
   },
