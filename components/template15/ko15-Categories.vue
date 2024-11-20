fa1f2bc1a0452132500bba1450fe973f0f6b962e
Cristian Fierro <cristianfierrosilva@gmail.com>
2024-11-19 20:44:38 -0500

    update: enhance ko15-Categories.vue with conditional rendering for swiper slides

--- components/template15/ko15-Categories.vue
+++ components/template15/ko15-Categories.vue
@@ -1,42 +1,65 @@
 <template>
   <div
-    class="w-full flex justify-center items-center py-25 lg:py-40 px-10"
+    class="flex w-full items-center justify-center px-10 py-25 lg:py-40"
     :style="`background-color: ${categories['--background_color_1']}`"
   >
     <div
-      class="w-full max-w-9/5 md:max-w-9/0 h-auto flex flex-col items-center justify-center overflow-hidden"
+      class="flex h-auto w-full max-w-9/5 flex-col items-center justify-center overflow-hidden md:max-w-9/0"
       :style="{ '--hover_text': settingGeneral?.hover_text }"
     >
       <p
-        class="w-full text-center mb-25 text-25 md:text-30 lg:text-40"
+        class="mb-25 w-full text-center text-25 md:text-30 lg:text-40"
         :style="`color: ${categories.color_title_1}; font-weight: ${categories.fontWeighTitle};`"
       >
         {{ categories.title }}
       </p>
-
       <div ref="mySwiper" v-swiper:mySwiper="swiperOption" class="w-full">
-        <div class="swiper-wrapper w-full">
+        <div v-if="dataStore?.id === 18376" class="swiper-wrapper w-full">
           <a
             v-for="(item, index) in categories.values"
             :id="`slide${index + 1}`"
             :key="index"
-            :href="item.url_redirect"
-            class="swiper-slide w-full wrapper-categories"
+            :href="index === 0 ? 'https://wa.link/labm4i' : item.url_redirect"
+            class="swiper-slide wrapper-categories w-full"
             rel="noreferrer noopener"
           >
             <img
               :src="idCloudinary(item.img, 550, 550)"
               alt="banner template15"
-              class="w-full h-full object-cover"
+              class="h-full w-full object-cover"
               :style="`border-radius: ${settingGeneral?.radius};`"
             />
             <div
-              class="mt-10 text-center text-16 md:text-20 lg:text-22 textHover"
+              class="textHover mt-10 text-center text-16 md:text-20 lg:text-22"
               :style="`color: ${item.color_title}; font-weight: ${item.fontWeighTitle};`"
             >
               <p>{{ item.title }}</p>
             </div>
           </a>
+        </div>
+        <div v-else class="swiper-wrapper w-full">
+          <a
+            v-for="(item, index) in categories.values"
+            :id="`slide${index + 1}`"
+            :key="index"
+            :href="item.url_redirect"
+            class="swiper-slide wrapper-categories w-full"
+            rel="noreferrer noopener"
+          >
+            <img
+              :src="idCloudinary(item.img, 550, 550)"
+              alt="banner template15"
+              class="h-full w-full object-cover"
+              :style="`border-radius: ${settingGeneral?.radius};`"
+            />
+            <div
+              class="textHover mt-10 text-center text-16 md:text-20 lg:text-22"
+              :style="`color: ${item.color_title}; font-weight: ${item.fontWeighTitle};`"
+            >
+              <p>{{ item.title }}</p>
+            </div>
+          </a>
+          <!--  -->
         </div>
         <div
           v-if="categories.values.length > 4"
